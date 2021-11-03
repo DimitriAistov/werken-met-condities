1419dc3fd3db218123fd17301eaa5fd5dca6552a
Dimitri <99058899@mydavinci.nl>
2021-11-03 12:09:22 +0100

    Min en Max

--- input en if-statement.py
+++ input en if-statement.py
@@ -1,6 +1,22 @@
 a = int(input("Waarde a? "))
 b = int(input("Waarde b? "))
 
+min = b
+max = a
+
+
 if a > b:
     max = a
     print("a is het grootste getal: " + str(max))
+
+elif a < b:
+    min = a
+    print("a is het kleinste getal: " + str(min))
+
+else:   print("a en b zijn even groot")
+
+
+
+print("het minimum is " + str(a) + " het maximum is " + str(b))
+
+
